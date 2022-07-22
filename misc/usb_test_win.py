303f14c2a64a643207211cf7333c7c67d84b8a81
Javier Joaquin <javierous@gmail.com>
2022-07-21 21:00:21 -0400

    Improve test script

--- misc/usb_test_win.py
+++ misc/usb_test_win.py
@@ -1,42 +1,58 @@
 #!/usr/bin/env python3
 
 import threading
+import struct
 from pywinusb import hid
+from enum import Enum
 
 
-done = threading.Event()
+class MessageType(Enum):
+    GetState = 0
+    SetState = 1
 
-# Whenever the host computer receives data from the
-# Mbed board, the received data is printed
+
 def on_data(data):
     print(f"Got message {data}")
+    (_, msg, on, warm, cool) = struct.unpack('<BB?HH', bytearray(data))
+
+    warm = (warm / (2 ** 16)) * 100.0
+    cool = (cool / (2 ** 16)) * 100.0
+
+    print(f"{MessageType(msg)} on: {on} warm: {warm:.2f} cool: {cool:.2f}")
     done.set()
 
 
-'''
-Gets all HIDs currently connected to host computer,
-and sets the first device with string "mbed" in its
-vendor name equal to variable mbed. This variable
-will be used to send data to the Mbed board.
-'''
+def send_set_command(report, on, warm, cool):
+    warm /= 100 / (2 ** 16)
+    cool /= 100 / (2 ** 16)
+    # The first byte is the report ID which must be 0
+    buffer = struct.pack('<BB?HH', 0, MessageType.SetState.value, on, int(warm), int(cool))
+    print(f"Sending {buffer}")
+    report.set_raw_data(buffer)
+    report.send()
+
+
+def send_get_command(report):
+    # The first byte is the report ID which must be 0
+    # Fields are ignored for this msg
+    buffer = struct.pack('<BB?HH', 0, MessageType.GetState.value, False, 0, 0)
+    report.set_raw_data(buffer)
+    report.send()
+
+
 all_hid_devices = hid.find_all_hid_devices()
 mbed_devices = [d for d in all_hid_devices if "mbed" in d.vendor_name]
 
 if not mbed_devices:
     raise ValueError("No HID devices found")
 
-# A buffer of bytes representing the values 1-8
-# The first byte is the report ID which must be 0
-#buffer = [0, 0, 0, 0, 0, 0]  # get
-buffer = [0, 1, 4, 0, 9, 0]  # set
+done = threading.Event()
 
 mbed_devices[0].open()
-# Set custom raw data handler
 mbed_devices[0].set_raw_data_handler(on_data)
 
-# Send the message to the Mbed board
 out_report = mbed_devices[0].find_output_reports()
-out_report[0].set_raw_data(buffer)
-out_report[0].send()
+send_set_command(out_report[0], True, 40.0, 15.0)
+# send_get_command(out_report[0])
 
 done.wait(5)
